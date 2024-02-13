f4ec49cafa62f7dc19437668be2478c64cc0764d
adnan-farid <faridadnan810@gmail.com>
2024-02-13 12:58:52 -0500

    lesson 5

--- REACT/01tut/src/Content.js
+++ REACT/01tut/src/Content.js
@@ -5,12 +5,25 @@ const HandleNameChange = () => {
   return names[int];
 }
 
+const handleClick = () => {
+  console.log('you clicked it.');
+}
+const handleClick2 = (name) => {
+  console.log(`${name} was clicked.`);
+}
+const handleClick3 = (e) => {
+  console.log(e.target.innerText);
+}
+
 const Content = () => {
   return (
     <main>
-      <p>
+      <p onDoubleClick={handleClick}>
         Hello {HandleNameChange()}!
       </p>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={() => {handleClick2('Adnan')}}>Click it 2</button>
+      <button onClick={(e) => {handleClick3(e)}}>Click it 3</button>
     </main>
   )
 }
