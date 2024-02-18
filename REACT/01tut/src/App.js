45bd01752883eb4a1ad4d361b70746d38689fdbd
adnan-farid <faridadnan810@gmail.com>
2024-02-18 13:19:16 -0500

    ch 14 commit

--- REACT/01tut/src/App.js
+++ REACT/01tut/src/App.js
@@ -4,6 +4,7 @@ import Content from './Content';
 import Footer from './Footer';
 import SearchItem from './SearchItem';
 import { useState, useEffect } from 'react';
+import apiRequest from './apiRequest';
 
 
 function App() {
@@ -33,29 +34,56 @@ function App() {
     }, 2000)
   }, [])
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
+
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
   }
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    setItems(listItems);  
+    setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ checked: myItem[0].checked })
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   }
-  const handleDelete = (id) => {
+
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-    setItems(listItems);
-  }   
+
+    const deleteOptions = { method: 'DELETE' };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!newItem) return;
-    
+    if (!newItem) return;
     addItem(newItem);
     setNewItem('');
   }
+  
   return (
     <div className="App">
       <Header title='Grocery List'/>
