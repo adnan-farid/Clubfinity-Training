95625ba76178c3810b3127b3afd2b144e765f946
adnan-farid <faridadnan810@gmail.com>
2024-02-18 18:33:21 -0500

    ch 23 commit

--- REACT/16tut/src/App.js
+++ REACT/16tut/src/App.js
@@ -6,11 +6,26 @@ import EditPost from './EditPost';
 import About from './About';
 import Missing from './Missing';
 import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import useAxiosFetch from './hooks/useAxiosFetch';
+import { useStoreActions } from 'easy-peasy';
 
 function App() {
+  const setPosts = useStoreActions((actions) => actions.setPosts);
+
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
+
+  useEffect(() => {
+    setPosts(data);
+  }, [data, setPosts])
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+      <Route
+          index
+          element={<Home isLoading={isLoading} fetchError={fetchError} />}
+        />
         <Route index element={<Home />} />
         <Route path="post">
           <Route index element={<NewPost />} />
