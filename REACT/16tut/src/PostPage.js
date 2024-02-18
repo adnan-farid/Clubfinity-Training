d36c0b69f0d41472903ca4a12bdc7b0931f055c3
adnan-farid <faridadnan810@gmail.com>
2024-02-18 17:53:02 -0500

    ch 21 commit

--- REACT/16tut/src/PostPage.js
+++ REACT/16tut/src/PostPage.js
@@ -1,8 +1,25 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import api from "./api/posts";
+import DataContext from "./context/DataContext";
 
-const PostPage = ({ posts, handleDelete }) => {
+const PostPage = () => {
+  const { posts, setPosts } = useContext(DataContext);
   const { id } = useParams();
+  const navigate = useNavigate();
   const post = posts.find((post) => post.id.toString() === id);
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      const postsList = posts.filter((post) => post.id !== id);
+      setPosts(postsList);
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
   return (
     <main className="PostPage">
       <article className="post">
@@ -14,10 +31,7 @@ const PostPage = ({ posts, handleDelete }) => {
             <Link to={`/edit/${post.id}`}>
               <button className="editButton">Edit Post</button>
             </Link>
-            <button
-              className="deleteButton"
-              onClick={() => handleDelete(post.id)}
-            >
+            <button className="deleteButton" onClick={() => handleDelete(post.id)}>
               Delete Post
             </button>
           </>
